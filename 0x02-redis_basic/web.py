6922c39dc260d39caed95a3de6dcc510d70100dd
Shola Onoriemu <pyshola@gmail.com>
2023-08-16 21:12:09 +0100

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,15 +16,14 @@ def count_url_access(method):
     def wrapper(url):
         cached_key = "cached:" + url
         cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
         count_key = "count:" + url
-        html = method(url)
-
         store.incr(count_key)
         store.set(cached_key, html)
         store.expire(cached_key, 10)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        html = method(url)
         return html
     return wrapper
 
